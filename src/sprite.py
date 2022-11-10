87f393c9ba9beda10c1ecc6162f99c7fe6156427
Jayson729 <jwilley729@gmail.com>
2022-11-10 07:16:52 -0500

    made new Sprite class will be used on everything that uses sprites

---
+++ src/sprite.py
@@ -0,0 +1,31 @@
+import pygame
+
+class Sprite(pygame.sprite.Sprite):
+
+    def __init__(self, img: pygame.Surface, x_pos, y_pos, color=None):
+        self.orig_image = img
+        self.orig_size = img.get_size()
+
+        self.image = img
+        self.rect = self.image.get_rect()
+
+        if color is not None:
+            self.color = color
+            color_image = pygame.Surface(
+                self.orig_size
+            ).convert_alpha()
+            color_image.fill(color)
+            self.image.blit(
+                color_image,
+                (0, 0),
+                special_flags=pygame.BLEND_RGBA_MULT
+            )
+        
+    """Resizes a sprite based on new width/height"""
+    def resize(self, new_width: int, new_height: int) -> None:
+        # scales image
+        self.image = pygame.transform.scale(
+            self.orig_image,
+            (new_width, new_height)
+        )
+        self.rect = self.image.get_rect(center=self.rect.center)
