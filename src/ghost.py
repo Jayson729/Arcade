56ecd758d897a53d82300a6b218ee546addb4e1e
Jayson729 <jwilley729@gmail.com>
2022-11-14 18:10:42 -0500

    added ghost.py with some comments

---
+++ src/ghost.py
@@ -0,0 +1,7 @@
+"""TODO: ghosts need to look in the direction they're moving
+They need some kind of AI/pathfinding algorithm 
+(probably A*, DFS/BFS if we're lazy)
+They'll all have the same algo, but they should ignore
+tiles that have a ghost on them so that they naturally
+go in different directions toward the player
+"""
